69fc263c8dc138482532f138abbc94b28b4eddcf
mogs.git <mogs.mail1@gmail.com>
2023-08-28 22:13:50 +0100

    updates to schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,17 +3,32 @@ import { PrismaClient } from '@prisma/client'
 const db = new PrismaClient()
 
 let defaultMarkers = [
-    { lat: 53.38, lng: -1.47 },
-    { lat: 53.374, lng: -1.479 },
-    { lat: 53.38, lng: -1.484 }
+    { lat: 53.38, lng: -1.47, markerEventType: "Near-Miss"},
+    { lat: 53.374, lng: -1.479, markerEventType: "Collision"},
+    { lat: 53.38, lng: -1.484, markerEventType: "Aggression" }
+]
+
+let markerTypes = [
+    {id: 1, markerEventTypeDetail: "Collision"},
+    {id: 2, markerEventTypeDetail: "Near-Miss"},
+    {id: 3, markerEventTypeDetail: "Aggression"},
 ]
 
 function seedMarkers() {
+    markerTypes.forEach(async type => {
+        console.log(type)
+        await db.markerType.create({
+            data: {
+                markerEventTypeDetail: type.markerEventTypeDetail
+            }
+        })
+    })
+    
     defaultMarkers.forEach(async m => {
-        await db.marker.create({data: {
+        await db.event.create({data: {
             latitude: m.lat,
             longitude: m.lng,
-            markerType: 1,
+            markerEventType: "Collision",
             description: ""
         }})
     })
